9f2f00ae8e4024959fcb19f905fd6f0c14245e50
IBS@ジョン <jeong-jin-young@ib-system.co.jp>
2024-08-09 16:58:47 +0900

    すべてのクライアントシークレットは、作成した直後のみユーザーへ表示

--- laravel-project/app/Providers/AppServiceProvider.php
+++ laravel-project/app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Passport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +22,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         // VerifyCsrfToken::except(['test/*']);
+        Passport::hashClientSecrets();
     }
 }
