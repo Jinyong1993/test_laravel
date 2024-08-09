c1e08519b6d183471b6ed4691b3bc9e19a254e91
IBS@ジョン <jeong-jin-young@ib-system.co.jp>
2024-08-09 16:02:08 +0900

    web.phpとは別のルートファイルを追加するならthen

--- laravel-project/bootstrap/app.php
+++ laravel-project/bootstrap/app.php
@@ -4,6 +4,7 @@ use App\Http\Middleware\Example;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,6 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: function () {
+            Route::middleware('web')->group(base_path('routes/foo.php'));
+        },
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->web(append: [
